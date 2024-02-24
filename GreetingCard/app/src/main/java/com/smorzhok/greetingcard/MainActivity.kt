5e03ad3ae7b4fadeb45c035efcc5b7ba99676e99
Vladislava <75379439+vladryanka@users.noreply.github.com>
2024-02-24 15:35:30 +0500

    Add files via upload

---
+++ GreetingCard/app/src/main/java/com/smorzhok/greetingcard/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.smorzhok.greetingcard
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.smorzhok.greetingcard.ui.theme.GreetingCardTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            GreetingCardTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Vladislava")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier:Modifier = Modifier) {
+    Surface(color = Color.Blue) {
+        Text(
+            text = "Hi, my name is $name!",
+            modifier = modifier.padding(24.dp)
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    GreetingCardTheme {
+        Greeting("Vladislava")
+    }
+}
